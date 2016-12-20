be9195349f887bc4a5e994fc473d06809dcc8c20
Magda *DarthTeddyBear* Skalicz <dark.ness.chaostwor@gmail.com>
2016-12-20 20:08:54 +0100

    Added a simplified representation of the playable character, as well as some environmental surfaces (ground and platforms). Currently, the playable character is unable to pass through any of them (collision detection for all sides of the platforms). Also, implemented the movement controls (including jumping with basic gravity mechanics).

---
+++ globvar.py
@@ -0,0 +1,21 @@
+# predefined global variables
+
+# fill colors
+BACKGROUND_FILL = (0, 128, 255)
+PLAYER_FILL = (255, 128, 0)
+SURFACE_FILL = (0, 153, 0)
+
+# screen size
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+PLAYER_SIZE = 60
+
+# player stride
+STRIDE = 7
+
+# ground level
+GROUND_LEVEL = 20
+
+# clock tick
+TICK = 60
+ 
--- main.py
+++ main.py
@@ -1,27 +1,75 @@
 # use pygame
-import pygame
+import pygame, player, envsurface, platformset1, globvar 
+ 
+def main():
+	# initialize pygame
+	pygame.init()
+ 
+	# create the game screen
+	screen = pygame.display.set_mode([globvar.SCREEN_WIDTH, globvar.SCREEN_HEIGHT])
+ 
+	# set the window title
+	pygame.display.set_caption('Mario')
 
-# screen size
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
-
-# initialize pygame
-pygame.init()
-
-# create the game screen
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-
-# set the window title
-pygame.display.set_caption('WiedzMario')
-
-exit_clicked = False
-
-# the event loop
-while not exit_clicked:
-	# process game events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_clicked = True
-
-# after (x) button is clicked
-pygame.quit()
+    # create the character
+	character = player.Player(globvar.GROUND_LEVEL, globvar.SCREEN_HEIGHT - globvar.GROUND_LEVEL - globvar.PLAYER_SIZE, globvar.PLAYER_SIZE, globvar.PLAYER_FILL)
+ 
+	# establish a link between player and level
+	level = platformset1.PlatformSet1(character)
+	character.level = level
+ 	
+ 	sprites = pygame.sprite.Group()
+ 	sprites.add(character)
+ 
+	exit_clicked = False
+ 
+	clock = pygame.time.Clock()
+	
+	# the event loop
+	while not exit_clicked:
+		# process game events
+		for event in pygame.event.get():
+            
+			if event.type == pygame.QUIT:
+				exit_clicked = True
+			# on key press
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					character.move_left()
+				elif event.key == pygame.K_RIGHT:
+					character.move_right()
+				elif event.key == pygame.K_UP:
+					character.jump()
+			# on key release
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT and character.change_x < 0:
+					character.stop()
+				elif event.key == pygame.K_RIGHT and character.change_x > 0:
+					character.stop()
+ 
+		# update the scene
+		sprites.update()
+		level.update()
+ 
+        # don't let the player leave the world
+		if character.rect.right > globvar.SCREEN_WIDTH:
+			character.rect.right = globvar.SCREEN_WIDTH
+ 
+		if character.rect.left < 0:
+			character.rect.left = 0
+ 
+        # draw the scene
+		level.draw(screen)
+		sprites.draw(screen)
+ 
+		# display the defined number of FPS
+		clock.tick(globvar.TICK)
+ 
+        # update the screen
+		pygame.display.flip()
+ 
+	# quit game upon clicking the (X) button
+	pygame.quit()
+ 
+if __name__ == "__main__":
+    main()
